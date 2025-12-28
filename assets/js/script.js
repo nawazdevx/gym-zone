0959d07c79986e642e0fc1d25be83742199e3503
nawazdevx <nawazdevx@gmail.com>
2025-12-28 07:38:15 +0500

    Add mobile layout and functionality

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ *  ────── Add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+/**
+ *  ────── Navbar toggle for mobile
+ */
+
+const navbar = document.querySelector('[data-navbar]');
+const navTogglers = document.querySelectorAll('[data-nav-toggler]');
+const navLinks = document.querySelectorAll('[data-nav-link]');
+
+const toggleNavbar = function () {
+  navbar.classList.toggle('active');
+};
+
+addEventOnElem(navTogglers, 'click', toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove('active');
+};
+
+addEventOnElem(navLinks, 'click', closeNavbar);
+
+/**
+ *  ────── Header & back top btn active
+ */
+
+const header = document.querySelector('[data-header]');
+const backTopBtn = document.querySelector('[data-back-top-btn]');
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 100) {
+    header.classList.add('active');
+    backTopBtn.classList.add('active');
+  } else {
+    header.classList.remove('active');
+    backTopBtn.classList.remove('active');
+  }
+});
